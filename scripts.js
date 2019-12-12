9959729e4042ac49d058d4e9c4b67af74fab002f
joshua <yoshiama12@gmail.com>
2019-12-11 20:56:08 -0700

    base js file

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// Replace ./data.json with your JSON feed
+fetch('./data.json')
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    // Work with JSON data here
+    console.log(data)
+  })
+  .catch(err => {
+    // Do something for an error here
+  })
